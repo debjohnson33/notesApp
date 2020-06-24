7952f40b91c0dc0378fe08fbb1988af5016b0e0c
Deb Johnson <friendswithdrj@gmail.com>
2020-06-24 18:31:00 -0500

    Add to comment

--- notes.js
+++ notes.js
@@ -1,4 +1,4 @@
-// User can create a note
+// User can create a note - How to persist a list of them?
 // User can edit a note
 // User can delete a note
 // When closing the browser window the notes will be stored and when the User returns, the data will be retrieved
