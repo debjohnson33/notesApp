5876139592f2d1ec4048f0badcada6dfeab956d9
Deb Johnson <friendswithdrj@gmail.com>
2020-06-24 18:14:33 -0500

    Edit form/createNote to persist note to local storage

--- notes.js
+++ notes.js
@@ -3,8 +3,10 @@
 // User can delete a note
 // When closing the browser window the notes will be stored and when the User returns, the data will be retrieved
 
-const createNote = (text) => {
-  localStorage.setItem("note": text);
-}
+const createNote = () => {
+  let text = document.getElementById("note-text").value;
+
+  window.localStorage.setItem("note", text);
+};
 
 
