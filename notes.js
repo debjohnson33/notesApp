8835dd74c4eb5810e7e815633d6f7afa4ed8ee7f
Deb Johnson <friendswithdrj@gmail.com>
2020-06-26 13:35:20 -0500

    Change notes to array to hand more than one note

--- notes.js
+++ notes.js
@@ -2,11 +2,12 @@
 // User can edit a note
 // User can delete a note
 // When closing the browser window the notes will be stored and when the User returns, the data will be retrieved
+const notes = JSON.parse(window.localStorage.getItem('notes')) || [];
 
 const createNote = () => {
   let text = document.getElementById("note-text").value;
-
-  window.localStorage.setItem("note", text);
+  notes.push(text);
+  window.localStorage.setItem("notes", JSON.stringify(notes));
 };
 
 
