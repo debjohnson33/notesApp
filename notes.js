668f1dad43e0f119d048a4b230ed2587a112506a
Deb Johnson <friendswithdrj@gmail.com>
2020-06-26 13:44:37 -0500

    Edit comments/add dom evenlistener for loading notes list

--- notes.js
+++ notes.js
@@ -1,13 +1,20 @@
-// User can create a note - How to persist a list of them?
+// User can create a note - How to persist a list of them - use stringify/parse to put array into localStorage - DONE
 // User can edit a note
 // User can delete a note
 // When closing the browser window the notes will be stored and when the User returns, the data will be retrieved
 const notes = JSON.parse(window.localStorage.getItem('notes')) || [];
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (notes !== []) {
+    console.log(notes);
+  }
+});
 
 const createNote = () => {
   let text = document.getElementById("note-text").value;
   notes.push(text);
   window.localStorage.setItem("notes", JSON.stringify(notes));
 };
+
 
 
