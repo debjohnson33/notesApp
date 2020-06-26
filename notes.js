30da2db253e8aa8a79cc4ad31c9309b62472d40b
Deb Johnson <friendswithdrj@gmail.com>
2020-06-26 14:55:02 -0500

    Edit button/editNote function

--- notes.js
+++ notes.js
@@ -6,14 +6,13 @@ const notes = JSON.parse(window.localStorage.getItem('notes')) || [];
 
 document.addEventListener("DOMContentLoaded", () => {
   if (notes !== []) {
-    console.log(notes);
     const list = document.getElementsByTagName("ul")[0];
     notes.map((note, index) => {
       let listItem = document.createElement("li");
       let editBtn = document.createElement("button");
       editBtn.innerHTML = "Edit";
-      editBtn.id = "btn-" + index;
-      editBtn.setAttribute("onclick", "editNote(event)");
+      editBtn.id = index;
+      editBtn.setAttribute("onclick", "editNote(event, this.id)");
       listItem.innerHTML = note;
       listItem.appendChild(editBtn);
       list.appendChild(listItem);
@@ -27,10 +26,18 @@ const createNote = () => {
   window.localStorage.setItem("notes", JSON.stringify(notes));
 };
 
-const editNote = () => {
+const editNote = (event, id) => {
   // retrieve index of current text in list
-
   // change that index to hold newText
+  console.log(id);
+  let index = Number(id);
+  let li = document.getElementById(index);
+  let inputBox = document.createElement("input");
+  inputBox.type = "text";
+  li.appendChild(inputBox);
+  let submit = document.createElement("button");
+  submit.innerHTML = "Submit";
+  inputBox.appendChild(submit);
 };
 
 
